6939e0848a66adada36dc0d61f523b78b5fc55cb
LordDusk <lang2619@gmail.com>
2019-08-24 11:19:47 +0200

    fixed prefix

--- bot.py
+++ bot.py
@@ -161,7 +161,7 @@ async def on_command_error(ctx, error):
 
     await ctx.send(
         f"Error: {str(error)}\nUh oh, that wasn't supposed to happen! "
-        f"Please join the Support Discord (#support) and tell the developer that: **{error_msg}!**")
+        f"Please join the Support Discord ($support) and tell the developer that: **{error_msg}!**")
     try:
         await owner.send(
             f"**{error_msg}**\n" \
