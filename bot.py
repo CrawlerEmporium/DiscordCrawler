24870db6a8612284894fb787007c001249c4b963
LordDusk <lang2619@gmail.com>
2019-11-19 09:33:09 +0100

    chore(fix): Listen to others.

--- bot.py
+++ bot.py
@@ -203,7 +203,7 @@ if __name__ == "__main__":
     bot.state = "run"
     for extension in [f.replace('.py', '') for f in listdir(GG.COGS) if isfile(join(GG.COGS, f))]:
         try:
-            if extension != "blacklist" and bot.testing == True:
+            if extension != "blacklist" and bot.testing == False:
                 bot.load_extension(GG.COGS + "." + extension)
         except Exception as e:
             log.error(f'Failed to load extension {extension}')
