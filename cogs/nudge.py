8a91c8fe1aa34f6b2c28b4b5fc1a802f4b580532
LordDusk <lang2619@gmail.com>
2020-02-05 17:46:53 +0100

    fix(bug): fixed cross channel nudging

--- cogs/nudge.py
+++ cogs/nudge.py
@@ -20,33 +20,33 @@ class Nudge(commands.Cog):
 
     @commands.command()
     @GG.is_staff()
-    async def nudge(self, ctx, messages: commands.Greedy[discord.Message]):
+    async def nudge(self, ctx, messages: commands.Greedy[int]):
+        msgs = []
+        channel = self.bot.get_channel(ctx.message.channel.id)
+        for message in messages:
+            try:
+                msg = await ctx.channel.fetch_message(message)
+                msgs.append(msg)
+                await msg.delete()
+            except:
+                for channel in ctx.guild.text_channels:
+                    perms = ctx.guild.me.permissions_in(channel)
+                    if channel == ctx.channel or not perms.read_messages or not perms.read_message_history:
+                        continue
+                    try:
+                        msg = await channel.fetch_message(message)
+                        msgs.append(msg)
+                        await msg.delete()
+                    except:
+                        continue
+                    else:
+                        break
+
         length = len(messages)
         if length <= 0:
             await ctx.send("I need one or more message Ids")
         else:
-            channel = self.bot.get_channel(messages[0].channel.id)
-            msgs = []
-            for message in messages:
-                try:
-                    msg = await ctx.channel.fetch_message(message.id)
-                    msgs.append(msg)
-                    await msg.delete()
-                except:
-                    for channel in ctx.guild.text_channels:
-                        perms = ctx.guild.me.permissions_in(channel)
-                        if channel == ctx.channel or not perms.read_messages or not perms.read_message_history:
-                            continue
-                        try:
-                            msg = await channel.fetch_message(message.id)
-                            msgs.append(msg)
-                            await msg.delete()
-                        except:
-                            continue
-                        else:
-                            break
-
-            message = f"{messages[0].author.mention} Your recent {singleOrMulti(length)[0]} should have been posted in this channel (see the channel description below).\n" \
+            message = f"{msgs[0].author.mention} Your recent {singleOrMulti(length)[0]} should have been posted in this channel (see the channel description below).\n" \
                       f"Your prior {singleOrMulti(length)[0]} {singleOrMulti(length)[1]} removed and {singleOrMulti(length)[2]} appended below. Thank you for helping keep conversations in their intended topics."
 
             embed = discord.Embed()
