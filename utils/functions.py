3d3ac01cb7546642783aefcd0a547d1ac7977dc1
LordDusk <lang2619@gmail.com>
2022-03-25 20:52:57 +0100

    kwargs localization attempt successfull!

--- utils/functions.py
+++ utils/functions.py
@@ -1,34 +1,34 @@
 import utils.globals as GG
 
 
-def get_name(cogName, command):
+def get_default_name(cogName, command):
     return GG.LOCALIZATION[cogName]['name'][command]['en-US']
 
 
-def get_description(cogName, command):
+def get_default_description(cogName, command):
     return GG.LOCALIZATION[cogName]['description'][command]['en-US']
 
 
-def get_names(cogName, command):
+def get_localized_names(cogName, command):
     return GG.LOCALIZATION[cogName]['name'][command]
 
 
-def get_descriptions(cogName, command):
+def get_localized_descriptions(cogName, command):
     return GG.LOCALIZATION[cogName]['description'][command]
 
 
 def get_command_kwargs(cogName, name):
-    return{
-        "name": get_name(cogName, name),
-        "description": get_description(cogName, name),
-        "name_localizations": get_names(cogName, name),
-        "description_localizations": get_descriptions(cogName, name)
+    return {
+        "name": get_default_name(cogName, name),
+        "description": get_default_description(cogName, name),
+        "name_localizations": get_localized_names(cogName, name),
+        "description_localizations": get_localized_descriptions(cogName, name)
     }
 
 
 def get_parameter_kwargs(cogName, name):
-    return{
-        "description": get_description(cogName, name),
-        "name_localizations": get_names(cogName, name),
-        "description_localizations": get_descriptions(cogName, name)
+    return {
+        "description": get_default_description(cogName, name),
+        "name_localizations": get_localized_names(cogName, name),
+        "description_localizations": get_localized_descriptions(cogName, name)
     }
