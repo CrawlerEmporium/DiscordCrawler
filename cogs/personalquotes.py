b1cad664f167ca6a1e2a6556948baf66fa2e50ad
LordDusk <lang2619@gmail.com>
2022-03-26 00:05:08 +0100

    Added method to get cogname

--- cogs/personalquotes.py
+++ cogs/personalquotes.py
@@ -36,30 +36,32 @@ class PersonalQuotes(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    cogName = 'personalquotes'
+    def get_cog_name(self):
+        return self.__class__.__name__.lower()
+
     personal = SlashCommandGroup("personal", "All your personal quotes")
 
-    @personal.command(**get_command_kwargs(cogName, "quote"))
-    async def quote(self, ctx, quote: Option(str, autocomplete=get_quote, **get_parameter_kwargs(cogName, "quote.quote"))):
+    @personal.command(**get_command_kwargs(get_cog_name(), "quote"))
+    async def quote(self, ctx, quote: Option(str, autocomplete=get_quote, **get_parameter_kwargs(get_cog_name(), "quote.quote"))):
         user_quote = await GG.MDB['personalcommands'].find_one({"user": ctx.interaction.user.id, "trigger": quote})
         await self.sendPersonalChoice(ctx, user_quote)
 
-    @personal.command(**get_command_kwargs(cogName, "clear"))
+    @personal.command(**get_command_kwargs(get_cog_name(), "clear"))
     async def clear(self, ctx):
         await GG.MDB['personalcommands'].delete_many({"user": ctx.interaction.user.id})
         await ctx.respond(content=":white_check_mark:" + ' **Cleared all your personal quotes.**')
 
-    @personal.command(**get_command_kwargs(cogName, "code"))
-    async def code(self, ctx, quote: Option(str, autocomplete=get_quote, **get_parameter_kwargs(cogName, "code.quote"))):
+    @personal.command(**get_command_kwargs(get_cog_name(), "code"))
+    async def code(self, ctx, quote: Option(str, autocomplete=get_quote, **get_parameter_kwargs(get_cog_name(), "code.quote"))):
         user_quote = await GG.MDB['personalcommands'].find_one({"user": ctx.interaction.user.id, "trigger": quote})
         replaceString = '\`'
         await ctx.respond(f"```{user_quote['response'].replace('`', replaceString)}```", files=user_quote['attachments'])
 
-    @personal.command(**get_command_kwargs(cogName, "add"))
+    @personal.command(**get_command_kwargs(get_cog_name(), "add"))
     async def add(self, ctx,
-                  quote: Option(str, **get_parameter_kwargs(cogName, "add.quote")),
-                  response: Option(str, **get_parameter_kwargs(cogName, "add.response")),
-                  attachment: Option(discord.Attachment, required=False, **get_parameter_kwargs(cogName, "add.attachment"))):
+                  quote: Option(str, **get_parameter_kwargs(get_cog_name(), "add.quote")),
+                  response: Option(str, **get_parameter_kwargs(get_cog_name(), "add.response")),
+                  attachment: Option(discord.Attachment, required=False, **get_parameter_kwargs(get_cog_name(), "add.attachment"))):
         checkIfExist = await GG.MDB['personalcommands'].find_one({"user": ctx.interaction.user.id, "trigger": quote})
         if checkIfExist is not None:
             return await ctx.respond(content=":x:" + ' **You already have a command with that trigger.**')
@@ -71,8 +73,8 @@ class PersonalQuotes(commands.Cog):
 
         await ctx.respond(content=":white_check_mark:" + ' **Command added.**')
 
-    @personal.command(**get_command_kwargs(cogName, "delete"))
-    async def delete(self, ctx, quote: Option(str, autocomplete=get_quote, **get_parameter_kwargs(cogName, "delete.quote"))):
+    @personal.command(**get_command_kwargs(get_cog_name(), "delete"))
+    async def delete(self, ctx, quote: Option(str, autocomplete=get_quote, **get_parameter_kwargs(get_cog_name(), "delete.quote"))):
         result = await GG.MDB['personalcommands'].delete_one(
             {"user": ctx.interaction.user.id, "trigger": quote.replace('\'', '\'\'')})
         if result.deleted_count > 0:
@@ -80,7 +82,7 @@ class PersonalQuotes(commands.Cog):
         else:
             await ctx.respond(content=":x:" + ' **Command with that trigger does not exist.**')
 
-    @personal.command(**get_command_kwargs(cogName, "list"))
+    @personal.command(**get_command_kwargs(get_cog_name(), "list"))
     async def list(self, ctx):
         user_quotes = await GG.MDB['personalcommands'].find({"user": ctx.interaction.user.id}).to_list(length=None)
         if len(user_quotes) == 0:
