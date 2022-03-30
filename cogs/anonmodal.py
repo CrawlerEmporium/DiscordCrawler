ba3945884623dd68096e12df3b15dff12cd7a399
LordDusk <lang2619@gmail.com>
2022-03-30 14:01:08 +0200

    added /anonmodal

---
+++ cogs/anonmodal.py
@@ -0,0 +1,68 @@
+import discord
+
+from discord import slash_command
+from discord.ext import commands
+
+from modals.Anonymous import Anonymous
+from utils import globals as GG
+from crawler_utilities.handlers import logger
+
+log = logger.logger
+
+
+class AnonModal(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(name="anonreport")
+    async def anonreport(self, ctx):
+        """Opens a modal to post an anonymous report."""
+        delivery_channel = await GG.MDB['channelinfo'].find_one({"guild": ctx.interaction.guild_id, "type": "DELIVERY"})
+        if delivery_channel is None:
+            await self.noDeliveryChannel(ctx.interaction.guild, ctx.interaction)
+            await self.notifyOwner(ctx.interaction.guild, ctx.interaction)
+
+        delivery_channel = await self.bot.fetch_channel(delivery_channel['channel'])
+        modal = Anonymous(self.bot, ctx.interaction, ctx.interaction.user, delivery_channel)
+        await ctx.interaction.response.send_modal(modal)
+
+    async def notifyOwner(self, guild, interaction_for_modal=None):
+        ServerOwner = self.bot.get_user(guild.owner.id)
+        if ServerOwner.dm_channel is None:
+            dm_channel = await ServerOwner.create_dm()
+        else:
+            dm_channel = ServerOwner.dm_channel
+        Embed = discord.Embed(title=f"No delivery channel exists.",
+                              description=f"Hello, a user of your server {guild.name} tried to make an "
+                                          f"anonymous report, but you have yet to make a delivery channel. You can do this by "
+                                          f"executing the following command: ``/addchannel <id> DELIVERY``. Thank you!")
+        try:
+            await dm_channel.send(embed=Embed)
+        except discord.Forbidden:
+            if interaction_for_modal is not None:
+                await interaction_for_modal.channel.send(
+                    f"Hello {guild.owner.mention}, a user just tried to make an anonymous report, but you have "
+                    f"yet to make a delivery channel. You can do this by executing the following command: ``/addchannel "
+                    f"<id> DELIVERY``. Thank you!")
+
+    async def noDeliveryChannel(self, guild, interaction_for_modal=None):
+        Reporter = self.bot.get_user(interaction_for_modal.user.id)
+        if Reporter.dm_channel is None:
+            dm_channel = await Reporter.create_dm()
+        else:
+            dm_channel = Reporter.dm_channel
+        Embed = discord.Embed(title=f"Error",
+                              description="Sorry this Server doesnt have a delivery channel yet, "
+                                          "I will notify the Server Owner for you if I'm able.")
+        try:
+            await dm_channel.send(embed=Embed)
+        except discord.Forbidden:
+            if interaction_for_modal is not None:
+                await interaction_for_modal.channel.send(
+                    "@Reporter, you have turned off the ability to let me DM you. I will notify the "
+                    "Server Owner that they haven't created a delivery channel yet.")
+
+
+def setup(bot):
+    log.info("[Cog] AnonModal Report")
+    bot.add_cog(AnonModal(bot))
---
+++ modals/Anonymous.py
@@ -0,0 +1,41 @@
+import discord
+from discord import InputTextStyle, Interaction
+from discord.ui import Modal, InputText
+from utils import globals as GG
+from crawler_utilities.handlers import logger
+from cryptography.fernet import Fernet
+
+log = logger.logger
+
+
+class Anonymous(Modal):
+    def __init__(self, bot, interaction, author, delivery_channel) -> None:
+        super().__init__(f"Anonymous Report")
+
+        self.bot = bot
+        self.interaction = interaction
+        self.author = author
+        self.delivery_channel = delivery_channel
+
+        self.add_item(InputText(label="Information", placeholder="What do you want to report to/share with the staff?", required=True, style=InputTextStyle.long))
+        self.add_item(InputText(label="Optional Links", placeholder="Insert links (if any) here", required=False, style=InputTextStyle.long))
+
+    async def callback(self, interaction: Interaction):
+        await interaction.response.defer(ephemeral=True)
+        embed = discord.Embed(title=f"New report", description=self.children[0].value)
+        if self.children[1].value != "":
+            embed.add_field(name="Optional Links", value=self.children[1].value)
+        message = await self.delivery_channel.send(embed=embed)
+
+        if message is not None:
+            embed.set_footer(text=f"Reply to this report by using • !reply {message.id} <your reply>")
+            await message.edit(embed=embed)
+            hashAuthor = f"{self.author.id}".encode()
+            f = Fernet(GG.KEY)
+            encrypt = f.encrypt(hashAuthor)
+            await GG.MDB['reports'].insert_one({"user": encrypt, "message": message.id})
+
+            await interaction.followup.send(f"Your report was successfully delivered to the staff!", ephemeral=True)
+        else:
+            await interaction.followup.send(f"Sorry, something went wrong while posting your report, please try again at a later stage.\n"
+                                            f"Or try contacting someone from staff directly, your name will not be shared with anyone else.", ephemeral=True)
